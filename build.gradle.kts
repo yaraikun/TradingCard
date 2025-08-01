6ab501020058ba8dd25f570dd9debb0ec5b88c2a
yaraikun <evan.calupig@gmail.com>
2025-08-01 13:18:28 +0800

    chore: added junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,23 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.guava)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+}
+
 application {
     mainClass.set("Main")
 }
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
